bd402bf18efdbdc903cc2e95f3f35124c9d81de0
j-hil <92876062+j-hil@users.noreply.github.com>
2023-01-18 23:35:17 +0000

    Draft `CodeLine` re-vamp

--- src/comform/codeline.py
+++ src/comform/codeline.py
@@ -1,3 +1,11 @@
+import tokenize
+from dataclasses import dataclass
+from pathlib import Path
+from tokenize import TokenInfo
+
+COMMENT = 61
+
+
 class CodeLine(str):
     """Representation of a line of code."""
 
@@ -31,3 +39,47 @@ class CodeLine(str):
         if new_col < old_col:
             raise ValueError("Tried to move a comment backward. Naughty")
         return CodeLine(self.prefix + " " * (new_col - old_col) + "#" + self.comment)
+
+
+@dataclass
+class CodeLine3:
+
+    text: str
+    comment_col: str
+
+    def __post_init__(self):
+        self.prefix = self.text[: self.comment_col]
+        self.comment = self.text[self.comment_col + 1 :]  # or self.comment.string
+
+        self.has_inline_comment = (self.prefix.strip() != "") and self.comment
+        self.has_own_line_comment = (self.prefix.strip() == "") and self.comment
+
+    def __repr__(self):
+        return self.__class__.__qualname__ + f"({self.text!r})"
+
+
+def main():
+
+    path = Path(__file__).parent.parent.parent / "tests" / "examples" / "align_bad.py"
+    with tokenize.open(path) as fh:
+        tokens = tokenize.generate_tokens(fh.readline)
+
+        code_lines: list[list[TokenInfo]] = []
+        comment_token: TokenInfo | None = None
+
+        for token in tokens:
+            if token.type == COMMENT:
+                comment_token = token
+            elif token.string == "\n":
+                comment_col = (
+                    comment_token.start[1] if comment_token else len(token.line)
+                )
+                code_lines.append(CodeLine3(token.line, comment_col))
+                comment_token = None
+
+    print("all lines:", *code_lines, sep="\n")
+    # print(code_lines[6])
+
+
+if __name__ == "__main__":
+    main()
