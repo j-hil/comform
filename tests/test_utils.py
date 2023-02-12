d22699d492dfb5b1cee60bfb0772b1def2b92bf1
j-hil <92876062+j-hil@users.noreply.github.com>
2023-02-12 01:52:43 +0000

    Expand & correct typing for `zip_padded`

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,7 +3,7 @@
 from comform.utils import zip_padded
 
 
-def test_zip_padded():
+def test_zip_padded() -> None:
     actual = list(zip_padded("abc", [1, 2, 3, 4, 5], (11, 12), fillvals=("x", 0, 11)))
     expected = [("a", 1, 11), ("b", 2, 12), ("c", 3, 11), ("x", 4, 11), ("x", 5, 11)]
     assert actual == expected
